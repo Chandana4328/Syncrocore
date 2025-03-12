7e9ece03c5ba81dce8a9db45a6b1742660f585d4
unknown <you@example.com>
2025-03-12 18:45:37 +0530

    second commit

---
+++ 12_03_2025.py
@@ -0,0 +1,23 @@
+#l="Hi there!"
+#l1=list(l)
+#print(l1)
+#-----------------------------------------------------------------------------------------------------------------------------
+'''alpha="abcd"#reverse the string
+rev=alpha[::-1]
+print(rev)
+
+
+
+a="Syncrocore"
+print(a[-1:])
+upper_string=a.upper()
+print(upper_string)
+lower_srting=a.lower()
+print(lower_srting)
+
+data="Hello Indian"
+for index in range(len(data)):
+    print(index,data[index])
+
+------------------------------------------------------------------------------------------------------'
+'''
