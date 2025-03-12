9f89f7f1d77bb6062fa75256607b8551713a94a9
unknown <you@example.com>
2025-03-12 15:34:39 +0530

    first commit

---
+++ pythoninten.py
@@ -0,0 +1,91 @@
+'''while True:
+     strng=input("enter a string(or 'QUIT' to exit):")
+     if strng.upper() == 'QUIT':
+        print("Exiting the program...")
+        break
+     lngth= len(strng)
+     print(f"The length of the string is:{lngth}")'''
+'''
+n=int(input())
+for i in range(i,n+1):
+    s=input("enter the string")
+    if len(s)<6:
+
+        print("enter again")
+    else:
+        print("longest word",s)
+'''
+'''list1=[11,22, 'k','v',"kv",450.67,True]
+
+
+print(list1)
+print(type(list1))'''
+'''lst1=[10,20,30]
+lst2=[40,50,60]
+lst3=lst1+lst2
+print(lst3)'''
+
+'''lst=[10,20,30,40,50,60]
+print(len(lst))
+print(min(lst))
+print(max(lst))
+print(sum(lst))
+print(any(lst))
+print(all(lst))
+lst=[25,55,35,15,40]
+del lst[2]
+print(lst)
+l=sorted(lst)
+lst.append(99)
+print(lst)
+r=lst.count(25)
+print(lst)
+#[11,22,11,44,11,66,11,88]
+print(lst.index(4))
+print(lst.index(4,3))
+print(lst.index(4,1,5))'''
+
+'''lst=[11,22,11,44,11,66,11,88]
+lst.insert(2,'k')
+print(lst)'''
+k=[11,22,11,44,11,66,11,88]
+k.sort()
+print(k)
+k.remove(11)
+print(k)
+k.reverse()
+print(k)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
